a1015b5ab7d304846c0a3dca18ce10bb347cdbf3
Giorgio Scibilia <giorgio.scibilia@renault.com>
2024-10-16 15:53:42 +0200

    modified bluetoothHIDManager

--- BluetoothHIDManager.swift
+++ BluetoothHIDManager.swift
@@ -10,34 +10,46 @@ import CoreBluetooth
 class BluetoothHIDManager: NSObject, CBPeripheralManagerDelegate {
     private var peripheralManager: CBPeripheralManager!
     private var hidService: CBMutableService!
-    
+    private var hidReportCharacteristic: CBMutableCharacteristic!
+
     override init() {
         super.init()
         peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
     }
-    
+
     func startAdvertising() {
         let advertisementData = [CBAdvertisementDataLocalNameKey: "iPhone Gamepad", CBAdvertisementDataServiceUUIDsKey: [CBUUID(string: "1812")]] as [String : Any]
         peripheralManager.startAdvertising(advertisementData)
+        print("Started advertising with data: \(advertisementData)")
     }
-    
+
     func stopAdvertising() {
         peripheralManager.stopAdvertising()
+        print("Stopped advertising")
     }
-    
+
     private func setupHIDService() {
         hidService = CBMutableService(type: CBUUID(string: "1812"), primary: true)
+
         let hidInformationCharacteristic = CBMutableCharacteristic(
             type: CBUUID(string: "2A4A"),
             properties: .read,
             value: Data([0x01, 0x01, 0x00, 0x03]),
             permissions: .readable
         )
-        hidService.characteristics = [hidInformationCharacteristic]
+
+        hidReportCharacteristic = CBMutableCharacteristic(
+            type: CBUUID(string: "2A4D"),
+            properties: [.notify, .read, .writeWithoutResponse],
+            value: nil,
+            permissions: [.readable, .writeable]
+        )
+
+        hidService.characteristics = [hidInformationCharacteristic, hidReportCharacteristic]
         peripheralManager.add(hidService)
         print("HID Service and Characteristics added.")
     }
-    
+
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         if peripheral.state == .poweredOn {
             print("Bluetooth is powered on.")
@@ -61,12 +73,20 @@ class BluetoothHIDManager: NSObject, CBPeripheralManagerDelegate {
     }
 
     private func sendHIDReport(report: [UInt8]) {
-        guard let hidReportCharacteristic = hidService.characteristics?.first else {
+        guard let hidReportCharacteristic = hidService.characteristics?.last else {
             print("HID Report Characteristic not found.")
             return
         }
         let data = Data(report)
-        peripheralManager.updateValue(data, for: hidReportCharacteristic as! CBMutableCharacteristic, onSubscribedCentrals: nil)
-        print("HID Report sent: \(report)")
+        let updated = peripheralManager.updateValue(data, for: hidReportCharacteristic as! CBMutableCharacteristic, onSubscribedCentrals: nil)
+        print("HID Report sent: \(report), Update success: \(updated)")
+    }
+
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
+        print("Central subscribed to characteristic: \(characteristic.uuid)")
+    }
+
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didUnsubscribeFrom characteristic: CBCharacteristic) {
+        print("Central unsubscribed from characteristic: \(characteristic.uuid)")
     }
 }
