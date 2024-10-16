996dc9ce21301c59c8740cfaf6cb19bdf1a9c506
Giorgio Scibilia <giorgio.scibilia@renault.com>
2024-10-16 14:15:39 +0200

    Initial project structure setup

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualGameController
+//
+//  Created by Giorgio Scibilia on 15/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
